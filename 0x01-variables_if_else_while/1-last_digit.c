778c32e6b34705d852366bb57018f69f918d9ea4
silas-stuart <silasbenza16@gmail.com>
2023-06-17 12:49:31 -0700

    Same fix to othrr files

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,15 +6,10 @@
 
 
 /**
-
  * main - Prints the last digit of a randomly generated number
-
  * and whether it is greater than 5, less than 6, or 0.
-
  *
-
  * Return: Always 0.
-
  */
 
 int main(void)
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,13 +2,9 @@
 
 
 /**
-
  * main - Prints the alphabet.
-
  *
-
  * Return: Always 0 (Success)
-
  */
 
 int main(void)
