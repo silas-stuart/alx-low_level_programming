1fb7a61faba6389b3d8cc3d608e5760b96fc64b2
silas-stuart <silasbenza16@gmail.com>
2023-06-29 06:49:51 -0700

    correction to the whitespaces of file 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,7 +10,7 @@
 int _strcmp(char *s1, char *s2)
 {
 	int res = 0;
-	
+
 	while (*s1 == *s2 && *s1 != '\0')
 	{
 		s1++;
@@ -18,6 +18,6 @@ int _strcmp(char *s1, char *s2)
 	}
 	if (s1 != s2)
 		res = *s1 - *s2;
-	
+
 	return (res);
 }
