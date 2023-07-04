733c16fe9043b3cafae29efbf9254715efdbcaf5
silas-stuart <silasbenza16@gmail.com>
2023-07-04 01:02:05 -0700

    a function that locates in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,18 +4,18 @@
  * _strchar - session execution
  * @s: input
  * @c: input2
- * Return: 0 (success)
+ * Return: the pointer to the first occurence of the character c.
  */
 
 char *_strchar(char *s, char c)
 {
-	int i = 0;
+	unsigned int i = 0;
 
-	for (; s[i] >= '0'; i++)
-	{
-		if (s[i] == c)
-			return (&s[i]);
-	}
+	for (; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
+			return (s + i);
+	if (*(s + i) == c)
+		return (s + i);
 	return (0);
 
 }
