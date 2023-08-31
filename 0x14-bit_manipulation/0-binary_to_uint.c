8ec7b39c34dcd8c2f5eba567d5da0491d4fc320c
silas-stuart <silasbenza16@gmail.com>
2023-08-31 09:34:10 -0700

    number conversion of base two to unsigned integer

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,17 +8,17 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int n = 0;
+	int expo;
+	unsigned int number = 0;
 
 	if (!b)
 		return (0);
 
-	for (i = 0; b[i]; i++)
+	for (expo = 0; b[expo]; expo++)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (b[expo] < '0' || b[expo] > '1')
 			return (0);
-		n = 2 * n + (b[i] - '0');
+		number = 2 * number + (b[expo] - '0');
 	}
-	return (n);
+	return (number);
 }
