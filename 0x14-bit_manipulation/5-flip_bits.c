c41863b658c7fb7efe3b49ef1a75b977ed52b843
silas-stuart <silasbenza16@gmail.com>
2023-08-31 10:59:16 -0700

    This function will return  the number of bits you would need to flip

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -16,7 +16,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 	for (intA = 63; intA >= 0; intA--)
 	{
-		longInt = exclusive >> i;
+		longInt = exclusive >> intA;
 		if (longInt & 1)
 			indexZero++;
 	}
